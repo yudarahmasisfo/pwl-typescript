02c79a1f209e2f7b25474a3562991651b7a81f0c
yudarahmasisfo <78396975+yudarahmasisfo@users.noreply.github.com>
2026-02-25 04:42:06 +0000

    update index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama) {
+    return 'Selamat Datang ' + nama + ' di PWL';
+}
+console.log(hello('Jon Down'));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+function hello(nama: string, umur: number): string{
+    return 'Selamat Datang '+nama+' di PWL, umur '+nama+' adalah = ' +umur;
+}
+const mahasiswa = "Andi";
+const umur:number = 19;
+console.log(hello(mahasiswa,umur));
+
+
+
